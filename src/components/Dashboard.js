c1facc52451135cdc4df916b061c5014de16d35a
PavanBandichode <pavan.bandichode@gmail.com>
2022-05-23 18:31:15 +0530

    Quiz-App

---
+++ src/components/Dashboard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Route, Routes, Fragment } from "react-router-dom";
+import Header from "./Header";
+import Category from "./Categoery";
+import Level from "./Level";
+import QuizHome from "./QuizHome";
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      category: null,
+      level: null,
+    };
+  }
+  handleCategory = (event, category) => {
+    this.setState({ category: category });
+  };
+  handleLevel = (event, level) => {
+    this.setState({ level: level });
+  };
+  render() {
+    return (
+      <>
+        <Header />
+
+        <Category
+          category={this.state.category}
+          handleCategory={this.handleCategory}
+        />
+        <Level
+          level={this.state.level}
+          handleLevel={this.handleLevel}
+          category={this.state.category}
+        />
+        <Routes>
+          <Route path="/quiz/:category/:level" strict element={<QuizHome />} />
+        </Routes>
+      </>
+    );
+  }
+}
+export default Dashboard;
